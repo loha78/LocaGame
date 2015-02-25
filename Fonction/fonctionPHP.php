7f2400250fc4057926c822b19630bc1d51ba2453
Merlinoir <jordane.beekman@gmail.com>
2015-02-25 17:39:50 +0100

    LocaGame : test connexion BDD

---
+++ Fonction/fonctionPHP.php
@@ -0,0 +1,6 @@
+<?php
+function connexion($nomBDD){
+	$co = mysqli_connect("localhost","root","",$nomBDD) or die ("erreur connexion BD");
+	return $co;
+}
+?>
--- listeJeu.php
+++ listeJeu.php
@@ -1,8 +1,6 @@
 <?php
-function connexion($nomBDD){
-	$co = mysqli_connect("localhost","root","",$nomBDD) or die ("erreur connexion BD");
-	return $co;
-}
+include "Fonction/fonctionPHP.php";
 
+ $co = connexion("locagame");
 
 ?>
