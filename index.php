78f9317ec5dd728c1419dd8b68cb1bd412dacdb3
Loha <t.loharano@gmail.com>
2015-02-25 14:45:26 +0100

    LocaGame : page d'accueil

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+
+	<head>
+		<title> LocaGame : Site de location de jeux videos </title>
+	</head>
+	
+	<body>
+		<div>
+			<form name="identify" action="#" method="post" >
+				<p>
+					<label>Login</label>
+						<input type="text" name="loginClient" />
+					<label>Mot de passe</label>
+						<input type="text" name="password" />
+					<input type="submit" value="Connexion" />
+				</p>
+			</form>
+		</div>
+		<div>
+			<a href="nouveauClient.php">Creer un nouveau compte</a>
+		</div>
+	
+	
+	
+	
+	</body>
+</html>
